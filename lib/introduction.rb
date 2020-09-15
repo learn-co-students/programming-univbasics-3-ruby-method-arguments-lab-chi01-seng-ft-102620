fcd5e27494184faefe8d8381428d7a8c14bab9aa
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-15 19:11:03 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,17 +5,18 @@
 
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
 
 
+def introduction_with_language(name, language)
+  puts"Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
 
-
-
-
-
-
-
-
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
 
 
 
